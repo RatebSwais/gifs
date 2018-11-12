c445b270ce41ae482581f7a682ec5351d08d9372
Rateb <rateb@localhost.localdomain>
2018-11-12 13:59:11 +0200

    search

--- gif.js
+++ gif.js
@@ -1,11 +1,15 @@
-// find elements
+// functiond elements
 var search = $("search")
 var button = $("button")
 // handle click and add class
 $(document).ready(function() {
 button.on("click", function(){
+  var x = document.getElementById("search").value
+  $("#search").hide()
+  $("#icon").hide()
+  button.hide()
   $.get({
-  url: "https://api.giphy.com/v1/gifs/search?q=kittens&api_key=gvXtdrXzNRducUeJnKFWP6I0UnRv8YN1&limit=25", 
+  url: "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=gvXtdrXzNRducUeJnKFWP6I0UnRv8YN1&limit=25", 
   success: function(result){
   var data = result.data
   var out="";
@@ -15,8 +19,8 @@ button.on("click", function(){
   console.log(url)
   out += "<img width='200px' src='"+url+"'/>"
   }
-  $("#search").html(out)
-console.log(result)
+  $(".container-2").html(out)
+  console.log(result)
   },
   error: function(error){
   console.log(error)
